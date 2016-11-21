627815bac0ff3f154db7c9701b071bf6e3a46ddf
freezaku <812029204@qq.com>
2016-11-20 19:29:24 -0800

    calculator assignment1

---
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by 徐鸿力 on 16/11/20.
+//  Copyright © 2016年 Honglix Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
