d4a040f961178a5bf435206c68db3671bfb23639
freezaku <812029204@qq.com>
2016-11-24 22:01:02 -0800

    uiview

---
+++ PartyRockApp/PartyRockApp/PartyCell.swift
@@ -0,0 +1,40 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by 徐鸿力 on 16/11/23.
+//  Copyright © 2016年 Honglix Xu. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handle the error
+            }
+        }
+    }
+
+}
