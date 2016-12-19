38a2bf27876e745b876eb3b93b3a96a32dd90935
freezaku <812029204@qq.com>
2016-12-18 21:43:37 -0800

    WeatherInHand

---
+++ WeatherInHand/Pods/Target Support Files/Pods-WeatherInHand/Pods-WeatherInHand-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WeatherInHandVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WeatherInHandVersionString[];
+
